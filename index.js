263380c7df70526c97b43eae53b130ddbfde4dc7
Abdul Ahad <abu290904@gmail.com>
2024-11-05 14:08:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Declaring 4 constants
+
+const num1 = 10;
+const num2 = 15;
+const num3 = 25;
+const num4 = 95;
+
+// Performing Addition Of num1 and num2
+let sum = num1 + num2
+
+// Displaying the result of sum on terminal
+console.log("The sum of "+ num1 +" + "+ num2 +" is "+ sum)
+
+// Performing Subraction of num3 and num1
+let subraction = num3 - num1
+
+// Displaying the result of subraction on terminal
+console.log("The difference of "+ num3 +" - "+ num1 +" is "+ subraction)
+
+// Performing multiplication of num3 and num4
+let multiplication = num3 * num4
+
+// Displaying the result of multiplication on terminal
+console.log("The product of "+ num3 +" * "+ num4 +" is "+ multiplication)
+
+// Performing divison of num4 and num1
+let divison = num4 / num1
+
+// Displaying the result of divison on terminal
+console.log("The division of "+ num4 +" / "+ num1 +" is "+ divison)
+
+
